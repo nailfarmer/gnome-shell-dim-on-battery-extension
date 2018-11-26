d4a3e0405dfd1e180c3405cfedb7d4ae99646811
Jen Bowen <nailfarmer@nailfarmer.com>
2018-11-26 16:19:19 +0000

    add toggle between preference options

--- dim-on-battery@nailfarmer.nailfarmer.com/prefs.js
+++ dim-on-battery@nailfarmer.nailfarmer.com/prefs.js
@@ -67,6 +67,33 @@ function buildPrefsWidget() {
 			}
 		});
 
+		let legacy_toggle_container = new Gtk.Box({
+			orientation: Gtk.Orientation.HORIZONTAL,
+			spacing: 20
+		});
+
+		let legacy_toggle_label = new Gtk.Label({
+			label: "Use legacy dim-by-value behavior",
+			use_markup: true,
+		});
+
+		var legacy_toggle_pref = config.LEGACY_MODE;
+		let legacy_toggle_value = new Gtk.Switch({active: legacy_toggle_pref.get()});
+	
+		legacy_toggle_value.connect('state-set', function(w, s, udata) {
+			var oldval = legacy_toggle_pref.get();
+			if (s != legacy_toggle_pref.get()) {
+				legacy_toggle_pref.set(s);
+                if ( s === true ) {
+                    dim_by_value_frame.set_sensitive(true);
+                    dim_by_percent_frame.set_sensitive(false);
+                } else {
+                    dim_by_value_frame.set_sensitive(false);
+                    dim_by_percent_frame.set_sensitive(true);
+                }
+			}
+		});
+
 
 		let ac_container = new Gtk.Box({
 			orientation: Gtk.Orientation.HORIZONTAL,
