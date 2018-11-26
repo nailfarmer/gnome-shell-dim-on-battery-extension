0d8c3f590c4b0b96932cd382529b5c55a7aa050c
Jen Bowen <nailfarmer@nailfarmer.com>
2018-11-26 16:34:58 +0000

    fix indentation

--- dim-on-battery@nailfarmer.nailfarmer.com/prefs.js
+++ dim-on-battery@nailfarmer.nailfarmer.com/prefs.js
@@ -10,30 +10,30 @@ function buildPrefsWidget() {
 	let mainBox = new Gtk.Box({
 		orientation: Gtk.Orientation.VERTICAL,
 		border_width: 10,
-	        spacing: 10
+		spacing: 10
 	});
 
 	(function() {
 
-        let dim_by_value_frame = new Gtk.Frame();
+		let dim_by_value_frame = new Gtk.Frame();
 		let dim_by_value_label = new Gtk.Label({
 			label: "Dim by value settings",
 			use_markup: true,
 		});
-        let dim_by_value_box = new Gtk.Box({
-            orientation: Gtk.Orientation.VERTICAL,
-            border_width: 10
-        });
+		let dim_by_value_box = new Gtk.Box({
+			orientation: Gtk.Orientation.VERTICAL,
+			border_width: 10
+		});
 
-        let dim_by_percent_frame = new Gtk.Frame();
+		let dim_by_percent_frame = new Gtk.Frame();
 		let dim_by_percent_label = new Gtk.Label({
 			label: "Dim by percent settings",
 			use_markup: true,
 		});
-        let dim_by_percent_box = new Gtk.Box({
-            orientation: Gtk.Orientation.VERTICAL,
-            border_width: 10
-        });
+		let dim_by_percent_box = new Gtk.Box({
+			orientation: Gtk.Orientation.VERTICAL,
+			border_width: 10
+		});
 
 		let battery_container = new Gtk.Box({
 			orientation: Gtk.Orientation.HORIZONTAL,
@@ -57,7 +57,7 @@ function buildPrefsWidget() {
 
 		var battery_pref = config.BATTERY_BRIGHTNESS;
 		let battery_value = Gtk.SpinButton.new_with_range(0,100,1);
-	        battery_value.set_value(battery_pref.get());	
+			battery_value.set_value(battery_pref.get());	
 		
 		battery_value.connect('value-changed', function(sw) {
 			var oldval = battery_pref.get();
@@ -84,16 +84,15 @@ function buildPrefsWidget() {
 			var oldval = legacy_toggle_pref.get();
 			if (s != legacy_toggle_pref.get()) {
 				legacy_toggle_pref.set(s);
-                if ( s === true ) {
-                    dim_by_value_frame.set_sensitive(true);
-                    dim_by_percent_frame.set_sensitive(false);
-                } else {
-                    dim_by_value_frame.set_sensitive(false);
-                    dim_by_percent_frame.set_sensitive(true);
-                }
+				if ( s === true ) {
+					dim_by_value_frame.set_sensitive(true);
+					dim_by_percent_frame.set_sensitive(false);
+				} else {
+					dim_by_value_frame.set_sensitive(false);
+					dim_by_percent_frame.set_sensitive(true);
+				}
 			}
 		});
-
 
 		let ac_container = new Gtk.Box({
 			orientation: Gtk.Orientation.HORIZONTAL,
@@ -112,7 +111,7 @@ function buildPrefsWidget() {
 
 		var ac_pref = config.AC_BRIGHTNESS;
 		let ac_value = Gtk.SpinButton.new_with_range(0,100,1);
-	        ac_value.set_value(ac_pref.get());	
+			ac_value.set_value(ac_pref.get());
 
 		ac_value.connect('value-changed', function(sw) {
 			var oldval = ac_pref.get();
@@ -130,18 +129,17 @@ function buildPrefsWidget() {
 		grid.attach_next_to(ac_value, ac_label, Gtk.PositionType.RIGHT, 1, 1);
 		*/
 
-        if ( legacy_toggle_pref.get() === true ) {
-            dim_by_value_frame.set_sensitive(true);
-            dim_by_percent_frame.set_sensitive(false);
-        } else {
-            dim_by_value_frame.set_sensitive(false);
-            dim_by_percent_frame.set_sensitive(true);
-        }
+		if ( legacy_toggle_pref.get() === true ) {
+			dim_by_value_frame.set_sensitive(true);
+			dim_by_percent_frame.set_sensitive(false);
+		} else {
+			dim_by_value_frame.set_sensitive(false);
+			dim_by_percent_frame.set_sensitive(true);
+		}
 
-        legacy_toggle_container.add(legacy_toggle_value);
-        legacy_toggle_container.add(legacy_toggle_label);
-        mainBox.add(legacy_toggle_container);
-
+		legacy_toggle_container.add(legacy_toggle_value);
+		legacy_toggle_container.add(legacy_toggle_label);
+		mainBox.add(legacy_toggle_container);
 
 		battery_container.add(battery_label);
 		battery_container.pack_end(battery_value, false, false, 0);
@@ -151,9 +149,9 @@ function buildPrefsWidget() {
 		ac_container.pack_end(ac_value, false, false, 0);
 		dim_by_value_box.add(ac_container);
 
-        	dim_by_value_frame.set_label("Dim by value settings");
-        	dim_by_value_frame.add(dim_by_value_box);
-        	mainBox.add(dim_by_value_frame);
+		dim_by_value_frame.set_label("Dim by value settings");
+		dim_by_value_frame.add(dim_by_value_box);
+		mainBox.add(dim_by_value_frame);
 	})();
 
 	mainBox.show_all();
